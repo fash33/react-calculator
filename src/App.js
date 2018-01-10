4fd2b670a8bae8d89932595cc2d114122a767252
Farhan <farhan.hamid39@gmail.com>
2018-01-11 02:34:17 +0600

    Modified CSS.

--- src/App.js
+++ src/App.js
@@ -6,15 +6,18 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Buttons />
       </div>
     );
+  }
+}
+class Buttons extends Component {
+  render() {
+    return(
+     <div className="App-Buttons">
+        <h1>My Calculator</h1>
+    </div>
+  )
   }
 }
 
